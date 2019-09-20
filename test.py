91ff05d628b7ac2ca20a0fa1e0fe6cb23658da9e
Lorenza Pacini <lorenza.pacini@gmail.com>
2019-09-20 13:29:07 +0200

    upload test

---
+++ test.py
@@ -0,0 +1,50 @@
+import osmnx as ox
+import networkx as nx
+import geopandas as gpd
+import numpy as np
+import shapely
+from shapely import speedups
+print('speedup available:', speedups.available)
+if speedups.available:
+    speedups.enable()
+import sec
+from edge_assigment import assign_edges
+import time
+
+point_coords = (45.745591, 4.871167) # latitude and longitude of Montplaisir-Lumière, Lyon (France)
+distance = 1000 # in meters
+buffer = 0.01 # 1 cm
+
+
+print("loading buildings...")
+B = ox.buildings_from_point(point_coords, distance=distance)
+print("buildings loaded.")
+
+B = ox.project_gdf(B) # pass from angular (lat, long) coords to planar coords
+B = B.geometry.buffer(buffer)
+
+def merge_and_convex(df):
+    go = True
+    length = len(df)
+    i = 0
+    print(i, length)
+    while go:
+        i += 1
+        df = gpd.GeoDataFrame(geometry=list(df.unary_union)).convex_hull
+        print(i, len(df))
+        if len(df) == length:
+            go = False
+        else:
+            length = len(df)
+    return df
+
+print('merging...')
+B = merge_and_convex(B)
+print('finished merging')
+
+nodes = B.centroid
+
+print('assigning edges...')
+t1 = time.time()
+edges1, weights1 = assign_edges(B)
+print('finished assigning edges in %s seconds' %(time.time() - t1))
